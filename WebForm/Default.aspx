220751f67deaef9e4d0f22f1dc160105189db9dd
lukelu6666 <lukelu@live.ca>
2016-11-14 21:29:19 -0500

    Fixed some bugs

--- WebForm/Default.aspx
+++ WebForm/Default.aspx
@@ -1,17 +1,38 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="WebForm.Web._Default" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
-        <title>Home</title>
+    <title>Home</title>
 
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
     <div class="jumbotron">
         <h2>Luke's Recipe Book</h2>
+        <p>
+            This is an assignment project for the course COMP229 Advance Web Application Develop.<br />
+        </p>
+        <p>Click to download Assignment instructions: <a href='~/documents/Assignment1.pdf'>#1</a>, <a href='~/documents/Assignment1.pdf'>#2</a>, <a href='~/documents/Assignment1.pdf'>#3</a></p>
+        <p>According the instructions, this project is using the below ASP.NET features. </p>
+        <ul>
+            <li>ASP.NET FORM Application</li>
+            <li>Master Page</li>
+            <li>Theme, Skin file</li>
+            <li>Change theme programmably</li>
+            <li>Users webcontrol</li>
+            <li>ADO.NET for Oracle</li>
+            <li>ADO.NET for SQL Server</li>
+            <li>Setting database server dynamicly</li>
+            <li>Bootstrap Css</li>
+            <li>Bootstrap remote modal method</li>
+        </ul>
+
     </div>
     <div class="row">
-          <div class="col-md-4">
+        <div class="col-md-4">
             <h3>About Luke</h3>
-            <p>I am Luke. I like cooking, therefor I searched and kept many recipes from magazine, online and other place. Here I share my recipe book to everyone.<br /><br /></p>
+            <p>
+                I am Luke. I like cooking, therefor I searched and kept many recipes from magazine, online and other place. Here I share my recipe book to everyone.<br />
+                <br />
+            </p>
             <img src="images/luke.jpg" class="img-circle" alt="Cinque Terre" width="304" height="236" />
         </div>
         <div class="col-md-4">
@@ -19,12 +40,18 @@
             <p>My Recipe Book is ONE place to store and organize the recipes Luke find and tear out of magazines, bookmark online, store on faded recipe cards – in a format that can be carried & used directly in the kitchen. </p>
             <img src="images/books.jpg" class="img-circle" alt="Cinque Terre" width="304" height="236" />
         </div>
-          <div class="col-md-4">
+        <div class="col-md-4">
             <h3>Setup</h3>
-            <p><a href="setup.aspx">Click here to setup a theme and database</a> <br /><br /><br /><br /></p>
+            <p>
+                <a href="setup.aspx">Click here to setup a theme and database</a>
+                <br />
+                <br />
+                <br />
+                <br />
+            </p>
             <img src="images/theme.png" class="img-circle" alt="Cinque Terre" width="304" height="236" />
         </div>
-    
+
 
     </div>
 </asp:Content>
--- WebForm/SQLScript/Oracle.sql
+++ WebForm/SQLScript/Oracle.sql
@@ -15,7 +15,7 @@ drop SEQUENCE sq_ingredient_id;
 	"CATEGORY" NVARCHAR2(50) NOT NULL,
 	"COOKINGTIME" NUMBER(5),
 	"NUMBEROFSERVINGS" NUMBER(5),
-	"DESCRIPTION" NVARCHAR2(500)  
+	"DESCRIPTION" NVARCHAR2(2000)  
    );
    
 --
@@ -64,4 +64,74 @@ BEGIN
   INSERT INTO RR_RECIPE VALUES (sq_recipe_id.nextval,v_name,v_submitby,v_category,v_cookingtime,v_numberOfServings,v_description);
   select sq_recipe_id.currval into v_recipeid from dual;
 END INSERT_RECIPE_SP;
+
+
+
+
+Insert into RR_RECIPE (ID,NAME,SUBMITBY,CATEGORY,COOKINGTIME,NUMBEROFSERVINGS,DESCRIPTION) values (sq_recipe_id.nextval,'Oracle-AWS','Luke','Breakfast',20,1,'Indicate this database is Oracle Database which is running on Amazon Web Service.');
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'test','1','cup',sq_recipe_id.currval);
+
+Insert into RR_RECIPE (ID,NAME,SUBMITBY,CATEGORY,COOKINGTIME,NUMBEROFSERVINGS,DESCRIPTION) values (sq_recipe_id.nextval,'Potato Gratin','Metro','Desserts',55,4,'In large saucepan, cook potatoes in boiling salted water 20 minutes or until tender. Drain and break up with fork.Stir in cheese, sour cream, butter and green onions; add salt and pepper to taste.Spread potato mixture evenly in a 1 1/2 qt (1.5 L) shallow rectangular baking dish or in four 1 1/2 cups (375 mL) shallow baking dishes.Combine bread crumbs and 1 Tbsp. (15 mL) melted butter; sprinkle evenly over potatoes. Bake 15 minutes or until heated through.');
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'P Potatoes, peeled, quartered','6','whole',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'Diced brie','1','cup',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'sour cream','3/4','cup',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'butter, softend','1/4','cup',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'finely chopped green onions','3','teas',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'fresh bread crumbs','1','cup',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'butter, melted','1','teaspoon',sq_recipe_id.currval);
+
+Insert into RR_RECIPE (ID,NAME,SUBMITBY,CATEGORY,COOKINGTIME,NUMBEROFSERVINGS,DESCRIPTION) values (sq_recipe_id.nextval,'Hawaiian Pita Lunch','Luke','Main dishes',30,1,'1. Preheat oven to 375 F. 2. Place pita bread on baking sheet. Spread sauce over pita. Top sauce with chicken, pineapple and green peppers. Sprinkle with cheese. 3. Bake for 7-10 minutes or until cheese is melted and pizza is bubbly.');
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'wheat pita','1','whole',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'mazzarella chess, shredded','1/2','cup',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'pizza sauce','2','teablespoon',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'cooked chicken breast','2 1/2','ounces',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'green peper, chopped','1/4','cup',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'fresh pineapple, diced','1/4','cup',sq_recipe_id.currval);
+
+Insert into RR_RECIPE (ID,NAME,SUBMITBY,CATEGORY,COOKINGTIME,NUMBEROFSERVINGS,DESCRIPTION) values (sq_recipe_id.nextval,'Cholay (Curried Chickpeas)','SHAMMI EDWARDS','Main dishes',45,1,'This is a very flavourful recipe from North India, usually eaten with fried bread like bhatura or puri. I like to serve it over rice for a very filling meal. You can also try it as a quick snack over toasted bread. This recipe is much quicker to make if you use precooked canned beans, but I like to think that the authentic flavour comes from doing it the hard way!');
+
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'Leaf','1','bay',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'Garbanzo Beans, drained','2','cans',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'Water','2','cups',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'Tea bag','1','bag',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'vegetable oil, divided','2','tablespoons',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'onion silce','1','whole',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'tomatoes, chopped','3','whole',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'fresh chilantro leaves','1/4','cup',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'cumin seeds','1','teaspoon',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'ground coriander','1','teaspoon',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'grated fresh ginger root','1','teaspoon',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'grated garlic','1','teaspoon',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'ground turmeric','1','teaspoon',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'onion, finely chopped','1','whole',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'ground cayenene pepper to taste','1','whole',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'masala','1','pinch garam',sq_recipe_id.currval);
+
+Insert into RR_RECIPE (ID,NAME,SUBMITBY,CATEGORY,COOKINGTIME,NUMBEROFSERVINGS,DESCRIPTION) values (sq_recipe_id.nextval,'Moroccan Lamb and Chickpea Stew Dinner','Luke','Main dishes',30,8,'The original recipe was written for 8 servings, and ingredient quantities from the original recipe are included in parentheses.');
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'boneless leg of lamb','3','ounces',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'ground cumin','1','teaspoon',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,' ground cinnamon','1','teaspoon',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'Pinch of salt','1/2','teaspoon',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,' vegetable oil','2','teaspoon',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'steamed carrot slices','1/4','cup',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'Pinch of chili powder','1/2','teaspoon',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'garlic clove, minced','1/3','whole',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'canned chickpeas, rinsed and drained','3 1/2','teaspoons',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'beef broth, low sodium ','3','tablespoons',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'ground coriander','1','teaspoon',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'canned crushed tomatoes','3 1/2','ounces',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'dried apricots','2','tablespoon',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'sugar ','1/8','teaspoon',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'cooked couscous','1/2','cup',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'ground ginger','1/8','teaspoon',sq_recipe_id.currval);
+Insert into RR_INGREDIENT (ID,NAME,QUANTITY,UNIT,RECIPEID) values (sq_ingredient_id.nextval,'medium onion, cut into thin wedges','1/8','each',sq_recipe_id.currval);
+commit;
+
+--for centennial College Oracle 1
+update rr_recipe set name='Centennial College Oracle 1', description='Indicate this database is Oracle Database which is running on Centennial College Oracle 1.' where id=1;
+commit;
+
+----for local Oracle
+update rr_recipe set name='local Oracle', description='Indicate this database is Oracle Database which is running on local Oracle.' where id=1;
+commit;
 
